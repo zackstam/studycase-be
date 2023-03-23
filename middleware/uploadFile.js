150b9076be81331e0b5e7c7e97f1c0d25f72e6b0
muhammad zakuan <zackstam@gmail.com>
2023-03-23 17:59:09 +0700

    refactoring file upload

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -2,21 +2,20 @@ const multer  = require('multer');
 const mime = require('mime');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/uploads/products')
-    },
-    filename: function (req, file, cb) {
-      cb(null, `product-${  Date.now()  }${Math.floor(Math.random() * 100)}.${  mime.getExtension(file.mimetype)}`);
-    }
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/products')
+  },
+  filename: function (req, file, cb) {
+    cb(null, `product-${  Date.now()  }${Math.floor(Math.random() * 100)}.${  mime.getExtension(file.mimetype)}`);
+  }
 })
-const upload = multer({ storage: storage });
 
-const uploadFile = (req, res, next) => {
-    upload.single('image')(req, res, () => {
-        req.body.image = req.file.filename || null
-        return next();
-    });
+const getFileName = (req, res, next) => {
+  req.body[req.file.fieldname] = req.file.filename || null
+  return next();
 }
+
 module.exports = {
-    uploadFile
+  storage,
+  getFileName
 }
--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const productController = require('../controllers/productController')
 const uploadFileMiddleware = require('../middleware/uploadFile');
-
+const multer  = require('multer');
 
 const router = express.Router();
+const upload = multer({ storage: uploadFileMiddleware.storage })
 
 // router.get('/paginate', productController.paginate);
 // router.get('/:pid',tagMiddleware.validateById, productController.byId)
 // router.get('/', productController.all);
-router.post('/', uploadFileMiddleware.uploadFile, productController.create);
+router.post('/', upload.single('image'), uploadFileMiddleware.getFileName, productController.create);
 // router.patch('/:pid', tagMiddleware.validateById, productController.update);
 // router.delete('/:pid', tagMiddleware.validateById, productController.destroy);
 
